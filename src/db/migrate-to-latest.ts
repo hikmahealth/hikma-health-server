c53f13454af58d775edf603a0cb759587d73c6ce
Ally Jr <3210jr@gmail.com>
2025-07-24 04:36:18 -0700

    feat: update migrations

--- src/db/migrate-to-latest.ts
+++ src/db/migrate-to-latest.ts
@@ -1,8 +1,9 @@
 import * as path from "path";
 import { promises as fs } from "fs";
-import { Migrator, FileMigrationProvider } from "kysely";
+import { Migrator, FileMigrationProvider, sql } from "kysely";
 import { fileURLToPath } from "url";
 import db from "./index";
+import { getMigrations } from "./migrations";
 
 // Get the directory path in ES modules
 const __filename = fileURLToPath(import.meta.url);
@@ -10,13 +11,34 @@ const __dirname = path.dirname(__filename);
 
 async function migrateToLatest() {
   console.log("Migrating to latest...", __dirname);
+
+  let latestMigrationId = null;
+  try {
+    const result =
+      await sql`SELECT version_num FROM alembic_version LIMIT 1`.execute(db);
+    latestMigrationId = result.rows[0]?.version_num;
+  } catch (error) {
+    // Table doesn't exist, fresh install
+    console.log("No alembic_version table found, running all migrations");
+  }
+
+  console.log("Latest migration id:", latestMigrationId);
+  console.log("Migrations:", getMigrations(latestMigrationId));
+
+  // TODO: Remove this return, just for testing
+  return;
+
   const migrator = new Migrator({
     db,
-    provider: new FileMigrationProvider({
-      fs,
-      path,
-      migrationFolder: path.join(__dirname, "migrations"),
-    }),
+    allowUnorderedMigrations: false,
+    provider: {
+      getMigrations: () => Promise.resolve(getMigrations(latestMigrationId)),
+    },
+    // provider: new FileMigrationProvider({
+    //   fs,
+    //   path,
+    //   migrationFolder: path.join(__dirname, "migrations"),
+    // }),
   });
 
   const { error, results } = await migrator.migrateToLatest();
--- src/db/migrations/index.ts
+++ src/db/migrations/index.ts
@@ -8,15 +8,21 @@ import * as addAppointmentsTable from "./20240901_add_appointments_table";
 import * as addPrescriptionsTable from "./20240926_add_prescriptions_table";
 import * as createServerVariablesTable from "./20250313_create_server_variables_table";
 import * as createResourcesTable from "./20250320_create_resources_table";
+import * as includeHealthcareProviderSegmentation from "./20250410_include_healthcare_provider_segmentation";
+import * as makeResourcesSyncable from "./20250401_make_resources_syncable";
+import * as restoringPatientAttributeIndicesAndPrimaryKeys from "./20240711_restoring_patient_attribute_indices_and_primary_keys";
+import { alembicMigrationIds } from "../alembic-migration-mapping";
 
-// Migration object that follows the Kysely migrations format
+/**
+ * Database migrations
+ */
 export const migrations = {
   // Migration name is the timestamp and name
   "20191125_initial_tables": {
     up: initialTables.up,
     down: initialTables.down,
   },
-  "20191125_initial_user": {
+  "20191126_initial_user": {
     up: initialUser.up,
     down: initialUser.down,
   },
@@ -27,6 +33,10 @@ export const migrations = {
   "20240603_patient_attribute_uuid_migration": {
     up: patientAttributeUuidMigration.up,
     down: patientAttributeUuidMigration.down,
+  },
+  "20240711_restoring_patient_attribute_indices_and_primary_keys": {
+    up: restoringPatientAttributeIndicesAndPrimaryKeys.up,
+    down: restoringPatientAttributeIndicesAndPrimaryKeys.down,
   },
   "20240821_increase_sex_text_length": {
     up: increaseSexTextLength.up,
@@ -48,13 +58,60 @@ export const migrations = {
     up: createResourcesTable.up,
     down: createResourcesTable.down,
   },
+  "20250401_make_resources_syncable": {
+    up: makeResourcesSyncable.up,
+    down: makeResourcesSyncable.down,
+  },
+  "20250410_include_healthcare_provider_segmentation": {
+    up: includeHealthcareProviderSegmentation.up,
+    down: includeHealthcareProviderSegmentation.down,
+  },
 };
 
-// Helper type for the database schema
+// TODO: Next migration - user capability based permission system.
+
+/**
+ * Helper type for the database schema
+ */
 export type Migration = {
   up: (db: Kysely<any>) => Promise<void>;
   down: (db: Kysely<any>) => Promise<void>;
 };
 
-// Helper function to get all migrations
-export const getMigrations = (): Record<string, Migration> => migrations;
+/**
+ * Returns all migrations that need to be run based on the latest alembic migration id
+ * @param latestAlembicMigrationId
+ * @returns Record of migrations to run
+ */
+export const getMigrations = (
+  latestAlembicMigrationId?: string,
+): Record<string, Migration> => {
+  // If no ID is provided, return all migrations
+  if (!latestAlembicMigrationId) {
+    return migrations;
+  }
+
+  const latestMigrationIndex = alembicMigrationIds.findIndex(
+    ([, id]) => id === latestAlembicMigrationId,
+  );
+  if (latestMigrationIndex === -1) {
+    throw new Error(
+      `Latest alembic migration id ${latestAlembicMigrationId} not found`,
+    );
+  }
+
+  const ignoredMigrationIds = alembicMigrationIds
+    .slice(0, latestMigrationIndex + 1)
+    .map(([key, _]) => key);
+
+  const migrationsToRun = {};
+
+  Object.entries(migrations).forEach(([key, value]) => {
+    if (!ignoredMigrationIds.includes(key)) {
+      // @ts-expect-error
+      migrationsToRun[key] = value;
+    }
+  });
+
+  return migrationsToRun;
+};
